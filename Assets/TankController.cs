dd54b7fb075192e967176cba87b48e33ac4f8745
Nathan Fonseca <airide101@gmail.com>
2019-02-09 18:49:50 -0500

    Modified tank origin.

--- Assets/TankController.cs
+++ Assets/TankController.cs
@@ -29,7 +29,7 @@ public class TankController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move();
         bool aimResult = CalculateAimTarget();
