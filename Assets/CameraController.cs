1e14070c1cfe49e0e0dca57d44382e3ca9003fe8
Nathan Fonseca <airide101@gmail.com>
2019-02-10 01:29:07 -0500

    Added particles explosion for shell on impact.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] [Range(1, 10)] float distance = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position + new Vector3(-25, 30, 25);
+        transform.position = target.position + new Vector3(-25, 30, 25) * distance;
         transform.LookAt(target);
     }
 }
--- Assets/Shell.cs
+++ Assets/Shell.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class Shell : MonoBehaviour
 {
+    [SerializeField] ParticleSystem explosion;
     Rigidbody rigidbody;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
-    
+
+    private void Update()
+    {
+        transform.LookAt(transform.position + rigidbody.velocity);
+    }
+
     public void ApplyForce(float velocity)
     {
         rigidbody = GetComponent<Rigidbody>();
-        rigidbody.velocity = transform.forward * velocity;
+        rigidbody.AddRelativeForce(new Vector3(0, 0, velocity));
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ParticleSystem thisExplosion = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(thisExplosion, 5f);
+        Destroy(gameObject);
     }
 }
