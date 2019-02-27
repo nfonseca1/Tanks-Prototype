a6bbf8ef7fc9c4c5a3e4f04ce5f4b5884812f603
Nathan Fonseca <airide101@gmail.com>
2019-02-27 18:48:59 -0500

    Created parachute and added AI detection to zone script.

--- Assets/Scripts/Zone.cs
+++ Assets/Scripts/Zone.cs
@@ -5,12 +5,50 @@ using UnityEngine;
 public class Zone : MonoBehaviour
 {
     public bool readyToAttack = false;
+    List<AIController> AI = new List<AIController>();
+    float time = 0f;
+    int numberOfAI = 0;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        if (time >= 1f)
+        {
+            numberOfAI = AI.ToArray().Length;
+
+            CheckForAI();
+        }
+    }
+
+    private void CheckForAI()
+    {
+        foreach (var tank in AI)
+        {
+            if (tank != null)
+            {
+                print("still here");
+            }
+            else
+            {
+                print("not here!");
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>() != null)
         {
             readyToAttack = true;
+        }
+
+        if (other.GetComponent<AIController>() != null)
+        {
+            if (!AI.Contains(other.GetComponent<AIController>()))
+            {
+                AI.Add(other.GetComponent<AIController>());
+                print(AI.ToArray().Length);
+            }
         }
     }
 }
