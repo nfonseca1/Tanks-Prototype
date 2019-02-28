a663cdd97044ab126bbe1a0e5557212c8ad7d36b
Nathan Fonseca <airide101@gmail.com>
2019-02-27 19:41:23 -0500

    Enemy falls slowly with parachute when spawned.

---
+++ Assets/Scripts/SpawnedEnemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnedEnemy : MonoBehaviour
+{
+    [SerializeField] Transform parachute;
+    CapsuleCollider collider;
+    Rigidbody rigidbody;
+    float fallSpeed = -10f;
+
+    private void Start()
+    {
+        collider = parachute.GetComponent<CapsuleCollider>();
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.MovePosition(transform.position + new Vector3(0, fallSpeed * Time.deltaTime, 0));
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(parachute.gameObject, .1f);
+        rigidbody.useGravity = true;
+        Destroy(this);
+    }
+}
