a104143f18f2bf2dfc54ba8ea238a8a97c859c63
Nathan Fonseca <airide101@gmail.com>
2019-02-28 20:10:26 -0500

    Added health bar.

--- Assets/Scripts/TankHealth.cs
+++ Assets/Scripts/TankHealth.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
     [SerializeField] float health = 100f;
     [SerializeField] Transform explosion;
-    
+    [SerializeField] Slider healthBar;
+    bool playerHealth = false;
+
+
+    private void Start()
+    {
+        if (GetComponent<PlayerController>() != null)
+        {
+            print("good");
+            playerHealth = true;
+        }
+    }
 
     public void DecreaseHealth(float damage)
     {
@@ -14,6 +26,15 @@ public class TankHealth : MonoBehaviour
         if (health <= 0)
         {
             BlowUp();
+        }
+    }
+
+    private void Update()
+    {
+        if (playerHealth)
+        {
+            healthBar.value = health / 100;
+            print("health val: " + healthBar.value);
         }
     }
 
