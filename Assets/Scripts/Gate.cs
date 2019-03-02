e1532ad473f985d8ad09186ea16d173e786c28d6
Nathan Fonseca <airide101@gmail.com>
2019-03-02 18:11:29 -0500

    Added gate script with animation.

---
+++ Assets/Scripts/Gate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] CapturePoint[] capturePoints;
+    Animation animation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    void OpenGate()
+    {
+        animation.Play();
+    }
+}
