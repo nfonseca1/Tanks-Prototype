0b84d17bbeeaac1e1bf935d0310cd05b2a2a5f8c
Nathan Fonseca <airide101@gmail.com>
2019-03-02 13:40:41 -0500

    Added health and gear powerups.

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(camera.transform.position);
+    }
+}
